3298bb87d38de45041828c4e77458c2e5ffe8e33
ale8276 <60494580+ale8276@users.noreply.github.com>
2020-02-13 08:40:28 +1300

    Display all students in a list

--- Credit Manager.py
+++ Credit Manager.py
@@ -1,6 +1,11 @@
 # Manage student info about L1 NCEA credits
 # 11/2/2020
 # Scenario: Mr. van Florentein needs a way to view how many L1 credits students currently have
+
 # This list stores student names
 students = ["Bob", "Mary", "Lewis", "John", "Steven"]
+
+# Display all students in a list
+for index in range(0, len(students)):
+    print(index + 1, students[index])
 
