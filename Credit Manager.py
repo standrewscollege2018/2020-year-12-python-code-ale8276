3487aef4bdd6921902bada35f97cd93846e55b7f
ale8276 <60494580+ale8276@users.noreply.github.com>
2020-02-13 08:40:06 +1300

    Create a list of student names

---
+++ Credit Manager.py
@@ -0,0 +1,6 @@
+# Manage student info about L1 NCEA credits
+# 11/2/2020
+# Scenario: Mr. van Florentein needs a way to view how many L1 credits students currently have
+# This list stores student names
+students = ["Bob", "Mary", "Lewis", "John", "Steven"]
+
