d97c5f84537c9d665ae748e371a33e4159f44386
ale8276 <60494580+ale8276@users.noreply.github.com>
2020-02-25 08:52:21 +1300

    Blank Check Functionality

--- Bookstore Task.py
+++ Bookstore Task.py
@@ -6,4 +6,23 @@ book_title = ["The Hunger Games", "To Kill a Mockingbird", "The Golden Compass"]
 book_author = ["Suzanne Collins", "Harper Lee", "Philip Pullman"]
 book_price = ["17.09","15.25","19.19"]
 
+# Function that will be used to check that the user inputs a string
+# This function will keep going until the user inputs a string
+def blank_check(text):
+    string_input = input(text)
+
+    # While loop that will keep going until a string is inputted
+
+    while True:
+        if string_input:
+            break
+
+        # If the user does not input anything, i.e blank input, then an error message will be printed out
+        # User will be prompted with an input
+
+        else:
+            print("Error! Please try again.")
+            string_input = input()
+
+    return string_input
 
