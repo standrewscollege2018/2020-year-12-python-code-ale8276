fd911c5d67737c353080a813db39becfb406cd48
ale8276 <60494580+ale8276@users.noreply.github.com>
2020-02-25 08:51:47 +1300

    List of book titles

---
+++ Bookstore Task.py
@@ -0,0 +1,9 @@
+# Bookstore Task
+# Write a program that would be used by an administrator to maintain the catalogue for a bookstore
+
+# List of book titles, author names and book prices
+book_title = ["The Hunger Games", "To Kill a Mockingbird", "The Golden Compass"]
+book_author = ["Suzanne Collins", "Harper Lee", "Philip Pullman"]
+book_price = ["17.09","15.25","19.19"]
+
+
